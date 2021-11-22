a3a76a2748b724ffa8448ea25007ce8f2d9e1c5d
Sanjit Sharma <beingsanjitsharma@gmail.com>
2021-11-22 18:23:04 -0500

    establishing the database connection

---
+++ initDB.js
@@ -0,0 +1,57 @@
+const newConnection = require("./DBConnection");
+
+let conn = newConnection();
+conn.connect();
+
+// Drop existing table (if there is one)
+conn.query("DROP TABLE Availability", (err, rows, fields) => {
+  if (err) console.log(err);
+  else console.log("Table Dropped");
+});
+
+// Create new table
+conn.query(
+  `
+    CREATE TABLE Availability 
+    (
+      Name varchar(100) NOT NULL PRIMARY KEY, 
+      LastUpdated timestamp, 
+      TimesAvailable json
+    )
+`,
+  (err, rows, fields) => {
+    if (err) console.log(err);
+    else console.log("Table Created");
+  }
+);
+
+// Insert row for admin into table, with 10 time slots
+conn.query(
+  `
+    INSERT INTO Availability VALUES ("Admin", CURRENT_TIME(), '["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]')
+    `,
+  (err, rows, fields) => {
+    if (err) console.log(err);
+    else console.log("One row inserted");
+  }
+);
+
+conn.query(
+  `
+    INSERT INTO Availability VALUES ("Bobby Joe", CURRENT_TIME(), '{"08:00":false, "09:00":true, "10:00":true, "11:00":true, "12:00":true, "13:00":false, "14:00":true, "15:00":true, "16:00":true, "17:00":true}')
+    `,
+  (err, rows, fields) => {
+    if (err) console.log(err);
+    else console.log("One row inserted");
+  }
+);
+
+/* Select the created row
+conn.query("SELECT * FROM Availability", (err, rows, fields) => {
+  if (err) console.log(err);
+  else console.log("One row selected");
+  //for (r of rows) console.log(r);
+});
+*/
+
+conn.end();
