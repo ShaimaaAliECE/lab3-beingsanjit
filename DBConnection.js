d9ccc77dbe78a60355092f8198a1ccc41e205e75
Sanjit Sharma <beingsanjitsharma@gmail.com>
2021-11-22 20:37:29 -0500

    minor formatting changes

--- DBConnection.js
+++ DBConnection.js
@@ -1,13 +1,16 @@
+//sshar374 Lab3
+//connecting to GCP server
+
 const mysql = require("mysql");
 
 function newConnection() {
-  let conn = mysql.createConnection({
-    host: "35.238.112.142",
-    user: "root",
-    password: "myPassword",
-    database: "myUsers",
-  });
-  return conn;
+    let conn = mysql.createConnection({
+        host: "35.238.112.142",
+        user: "root",
+        password: "myPassword",
+        database: "myUsers",
+    });
+    return conn;
 }
 
 module.exports = newConnection;
